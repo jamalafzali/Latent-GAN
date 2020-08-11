f2e5cd903e685b57d9abf4f3cadbe63bb07dd992
Jamal Afzali <ja819@ray18.doc.ic.ac.uk>
2020-08-12 00:07:54 +0100

    Structured converter

---
+++ AE-GAN VF/convertToStructuredMesh.py
@@ -0,0 +1,73 @@
+# https://github.com/pyvista/pyvista-support/issues/148
+
+#   X Range: -359.685 to 359.685 (delta: 719.369)
+#   Y Range: -338.124 to 338.124 (delta: 676.247)
+#   Z Range: 0.2 to 250 (delta: 249.8)
+
+import numpy as np
+import sys
+import vtktools
+import pyvista as pv
+
+def get_structured_velocity(fileNumber):
+    sys.path.append('fluidity-master')
+    fileName = '/vol/bitbucket/ja819/Fluids Dataset/small3DLSBU/LSBU_' + str(fileNumber) + '.vtu'
+    mesh = pv.read(fileName)
+
+    size = 64
+    x = np.linspace(-359.69, 359.69, size)
+    y = np.linspace(-338.13, 338.13, size)
+    z = np.linspace(0.2, 250, size)
+    x, y, z = np.meshgrid(x, y, z)
+
+    grid = pv.StructuredGrid(x, y, z)
+    result = grid.interpolate(mesh, radius=20.)
+    p = result.point_arrays['Velocity']
+    p = p.transpose()
+    return p
+
+p = get_structured_velocity(500)
+print(p.shape)
+
+def convert_to_structured(data):
+    sys.path.append('fluidity-master')
+    fileName = '/vol/bitbucket/ja819/Fluids Dataset/small3DLSBU/LSBU_0.vtu'
+    mesh = pv.read(fileName)
+
+    size = 64
+    x = np.linspace(-359.69, 359.69, size)
+    y = np.linspace(-338.13, 338.13, size)
+    z = np.linspace(0.2, 250, size)
+    x, y, z = np.meshgrid(x, y, z)
+
+    grid = pv.StructuredGrid(x, y, z)
+    result = grid.interpolate(mesh, radius=20.)
+    result.point_arrays['Velocity'] = data   
+
+    foo = mesh.copy()
+    foo.clear_arrays()
+    result2 = foo.sample(result)
+
+    p = result2.point_arrays['Velocity']
+    return p
+
+# #######################################
+# # Convert existing data to Structured #
+# #######################################
+# for fileNumber in range(988,989):
+
+#     sys.path.append('fluidity-master')
+#     fileName = '/vol/bitbucket/ja819/Fluids Dataset/small3DLSBU/LSBU_' + str(fileNumber) + '.vtu'
+#     mesh = pv.read(fileName)
+
+#     size = 64
+#     x = np.linspace(-359.69, 359.69, size)
+#     y = np.linspace(-338.13, 338.13, size)
+#     z = np.linspace(0.2, 250, size)
+#     x, y, z = np.meshgrid(x, y, z)
+
+#     grid = pv.StructuredGrid(x, y, z)
+#     result = grid.interpolate(mesh, radius=20.)
+
+#     result.save('/vol/bitbucket/ja819/Fluids Dataset/small3DLSBU-Structured/LSBU_' + str(fileNumber) + '.vtk')
+#     print(fileNumber)
