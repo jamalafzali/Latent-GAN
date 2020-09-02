4baf888286977a38cf25d36890f9a671de372562
Jamal <ja819@ic.ac.uk>
2020-09-02 17:57:33 +0100

    Clean up

--- Main Files/Norm.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-def normalise(x, x_min, x_max):
-    #x = x.numpy()
-    return (x - x_min) / (x_max - x_min)
-
-def denormalise(x_norm, x_min, x_max):
-    #x_norm = x_norm.numpy()
-    return (x_norm*x_max - x_min) + x_min
--- getValidationSet.py
+++
@@ -1,8 +0,0 @@
-# Splitting data between train and validation sets
-ints = list(range(time_steps))
-random.shuffle(ints)
-int_to_split = int(val_percent * time_steps)
-train_ints = ints[int_to_split:]
-val_ints = ints[:int_to_split]
-
-print()
